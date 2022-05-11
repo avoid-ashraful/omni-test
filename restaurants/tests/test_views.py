e480eef6c784729acc221bcc3e68783a75a313ac
Ashraful Islam <avoid.ashraful@gmail.com>
2022-05-11 21:10:35 +0600

    Add tests for top list restaurant api

--- restaurants/tests/test_views.py
+++ restaurants/tests/test_views.py
@@ -1,10 +1,15 @@
 import pytest
 
 from datetime import datetime, timedelta
+from factory import create_batch
 
 from django.urls import reverse
 from restaurants.models import Restaurant, RestaurantTimeSlot
-from restaurants.tests.factories import RestaurantFactory, RestaurantTimeSlotFactory
+from restaurants.tests.factories import (
+    MenuFactory,
+    RestaurantFactory,
+    RestaurantTimeSlotFactory,
+)
 from rest_framework import status
 
 
@@ -59,3 +64,31 @@ class TestRestaurantViews(Base):
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.data.get("count") == 0
+
+
+class TestRestaurantMenuListViews(Base):
+    @pytest.fixture
+    def url(self):
+        return reverse(
+            "api:restaurants:list-menu",
+        )
+
+    def test_restaurant_top_list_api(self, client, url, restaurant):
+        menus = MenuFactory.create_batch(size=10, restaurant=restaurant)
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data.get("results", [{}])[0].get("no_of_menus")
+
+    def test_restaurant_top_list_api_with_params_less(self, client, url, restaurant):
+        menus = MenuFactory.create_batch(size=10, restaurant=restaurant)
+        response = client.get(f"{url}?eq=less&no_of_dish=11")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data.get("count") == 1
+        assert response.data.get("results", [{}])[0].get("no_of_menus") == 10
+
+    def test_restaurant_top_list_api_with_params_more(self, client, url, restaurant):
+        menus = MenuFactory.create_batch(size=10, restaurant=restaurant)
+        response = client.get(f"{url}?eq=more&no_of_dish=9")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data.get("count") == 1
+        assert response.data.get("results", [{}])[0].get("no_of_menus") == 10
